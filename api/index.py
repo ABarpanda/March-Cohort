49e3aef569b743f37730921bccc094e04e198734
Amritanshu Barpanda <145291762+ABarpanda@users.noreply.github.com>
2025-03-30 09:50:30 +0530

    render trial 2

--- api/index.py
+++ api/index.py
@@ -111,5 +111,5 @@ async def get_itinerary(
         )
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))  # Railway assigns PORT dynamically
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    port = int(os.environ.get("PORT", 10000))  # Ensure it uses Render's default port
+    uvicorn.run(app, host="0.0.0.0", port=port)
