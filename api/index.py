b69ee076940eec93e0edcd2ddcbda7275de082b5
Amritanshu Barpanda <145291762+ABarpanda@users.noreply.github.com>
2025-03-30 08:52:20 +0530

    railway trial 3

--- api/index.py
+++ api/index.py
@@ -112,4 +112,4 @@ async def get_itinerary(
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))  # Railway assigns PORT dynamically
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=port, reload=True)
